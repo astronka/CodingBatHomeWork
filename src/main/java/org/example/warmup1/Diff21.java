162aa10854c4ff718551648d0df715b4d69d4ef8
Astra <clementmodigliani@icloud.com>
2026-07-21 17:45:59 +0600

    Diff21

---
+++ src/main/java/org/example/warmup1/Diff21.java
@@ -0,0 +1,12 @@
+package org.example.warmup1;
+
+public class Diff21 {
+    public int diff21(int n) {
+
+        if (n <= 21) {
+            return (21 - n);
+        } else {
+            return ((n - 21) * 2);
+        }
+    }
+}
