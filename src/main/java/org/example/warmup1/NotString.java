92ec84603ec5364813043f3fb3914c9e024e947b
Astra <clementmodigliani@icloud.com>
2026-07-21 21:05:37 +0600

    NotString

---
+++ src/main/java/org/example/warmup1/NotString.java
@@ -0,0 +1,11 @@
+package org.example.warmup1;
+
+public class NotString {
+    public String notString(String str) {
+        if ( str.startsWith("not")) {
+            return str ;
+        } else {
+            return "not " + str;
+        }
+    }
+}
