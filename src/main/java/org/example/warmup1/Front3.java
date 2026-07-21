d5474ae4a701cfbb35ec538a3a8081b8a3135f52
Astra <clementmodigliani@icloud.com>
2026-07-21 21:45:47 +0600

    Front3

---
+++ src/main/java/org/example/warmup1/Front3.java
@@ -0,0 +1,15 @@
+package org.example.warmup1;
+
+public class Front3 {
+    public String front3(String str) {
+        if (str.length() >= 3) {
+
+            return str.substring(0, 3) + str.substring(0, 3) + str.substring(0, 3);
+
+        } else {
+
+            return str + str + str;
+        }
+    }
+
+}
