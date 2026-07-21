88b58cff521cb163e4fe29d651e319580c52cfbe
Astra <clementmodigliani@icloud.com>
2026-07-21 18:29:58 +0600

    NearHundred

---
+++ src/main/java/org/example/warmup1/NearHundred.java
@@ -0,0 +1,10 @@
+package org.example.warmup1;
+
+public class NearHundred {
+    public boolean nearHundred(int n) {
+        if ( Math.abs(100 - n) <= 10 || Math.abs( 200-n) <= 10) {
+            return true ;
+        }
+        return false ;
+    }
+}
