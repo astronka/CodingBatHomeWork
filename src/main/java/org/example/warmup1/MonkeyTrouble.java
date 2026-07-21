e028f68989aa0f342ab1bb821c3743e571be1d69
Astra <clementmodigliani@icloud.com>
2026-07-21 17:20:06 +0600

    MonkeyTrouble

---
+++ src/main/java/org/example/warmup1/MonkeyTrouble.java
@@ -0,0 +1,10 @@
+package org.example.warmup1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile || !aSmile && !bSmile){
+            return true;
+        }
+        return false;
+    }
+}
