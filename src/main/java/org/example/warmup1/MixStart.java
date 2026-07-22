f5bcce12fb920904faf9c4b627ab2c94146683eb
Astra <clementmodigliani@icloud.com>
2026-07-22 17:09:16 +0600

    MixStart

---
+++ src/main/java/org/example/warmup1/MixStart.java
@@ -0,0 +1,9 @@
+package org.example.warmup1;
+
+public class MixStart {public boolean mixStart(String str) {
+    if ( str.startsWith("ix",1)) {
+        return true;
+    }
+    return false;
+}
+}
