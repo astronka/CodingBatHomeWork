626cab712c64b940e4f4b53780b7f1af7d88b9f1
Astra <clementmodigliani@icloud.com>
2026-07-22 18:11:40 +0600

    max1020

---
+++ src/main/java/org/example/warmup1/Max1020.java
@@ -0,0 +1,15 @@
+package org.example.warmup1;
+
+public class Max1020 {
+    public int max1020(int a, int b) {
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
+}
