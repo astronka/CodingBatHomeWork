7348271b8d62203761dd18b1888d3de57485e51b
Astra <clementmodigliani@icloud.com>
2026-07-22 12:37:11 +0600

    Front22

---
+++ src/main/java/org/example/warmup1/Front22.java
@@ -0,0 +1,12 @@
+package org.example.warmup1;
+
+public class Front22 {
+
+    public String front22(String str) {
+        if ( str.length() >2) {
+            return str.substring(0,2) + str + str.substring(0,2);
+        }
+        return str+str+str;
+    }
+
+}
